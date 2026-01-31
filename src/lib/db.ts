db6a475d4441e11b97f4443c8f32019c6ca0347c
subodha-wijesekara <wijesekararsc@gmail.com>
2026-02-01 02:39:27 +0530

    get ready to deploy

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
+
 
 interface MongooseCache {
   conn: typeof mongoose | null;
@@ -24,6 +20,12 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
